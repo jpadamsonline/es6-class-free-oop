911cda12802c5ef0328542c584ee7be3f91d0f78
JP Adams <jpadamsonline@gmail.com>
2017-04-15 09:50:06 -0500

    Create animals.js
    Checking in my animal kingdom

---
+++ animals.js
@@ -0,0 +1,48 @@
+function newAnimal(spec = {}){
+  let { name = 'it', word = 'nothing' } = spec,
+    talk = () => console.log( name + ' says ' + word);
+  return Object.freeze({ name, word, talk });
+}
+
+function newAnimalMovement(spec = {}) {
+  let { name = 'it' } = spec,
+    fly = () => console.log(name + ' is flying: flap flap'),
+    swim = () => console.log(name + ' is swimming: splish splash'),
+    walk = () => console.log(name + ' is walking: stomp stomp');
+  return Object.freeze({ fly, swim, walk });
+}
+
+function newAlligator(spec){
+  let { name, word, talk } = newAnimal(spec),
+    { walk, swim } = newAnimalMovement(spec);
+  return Object.freeze({ name, word, talk, walk, swim });
+}
+
+function newDuck(spec = {}){
+  // overide defaults
+  let { name = 'duck', word = 'quack' } = spec ,
+    { talk } = newAnimal({ name, word }),
+    { fly, walk, swim } = newAnimalMovement({ name });
+  return Object.freeze({ name, word, talk, fly, walk, swim });
+}
+
+//////////////////////////////////////
+
+let typicalGator = newAlligator({ name: 'alligator', word: 'grrrr' });
+typicalGator.talk();
+typicalGator.swim();
+
+let noNameMuteGator = newAlligator();
+noNameMuteGator.talk();
+noNameMuteGator.walk();
+
+let duck = newDuck();
+duck.talk();
+duck.fly();
+
+let donald = newDuck({name: 'Donald', word: 'qua qua quaaack'});
+donald.talk();
+duck.swim();
+
+let animal = newAnimal({name: 'Animal from the Muppets'});
+animal.talk();
